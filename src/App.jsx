7c37ff6b29a8f185c5e2c9c35114f54a84c90589
ThomasLonjon <lonjon.thomas@gmail.com>
2023-05-11 20:22:17 +0200

    added map

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import MyMap from "./components/MyMap";
 
 function App() {
+  const [location, setLocation] = useState(null);
+  const [isChosen, setIsChosen] = useState(null);
+  const [latitude, setLatitude] = useState(2.136888191793237);
+  const [longitude, setLongitude] = useState(43.707299293319366);
+
+  const handleChange = (result) => {
+    setLocation(result);
+    setLatitude(result.center[0]);
+    setLongitude(result.center[1]);
+    setIsChosen(true);
+  };
+
   return (
     <>
       <div className="mapdiv">
-        <MyMap latitude={2.136888191793237} longitude={43.707299293319366} />
+        <MyMap
+          latitude={latitude}
+          longitude={longitude}
+          handleChange={handleChange}
+          isChosen={isChosen}
+        />
       </div>
     </>
   );
